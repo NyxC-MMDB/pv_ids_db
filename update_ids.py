f5cb976e2fc5991ff462fa8a68971f65173157a0
NyxC <esteesunweyrandom@gmail.com>
2025-11-15 20:46:01 -0500

    Update update_ids.py

--- update_ids.py
+++ update_ids.py
@@ -12,27 +12,53 @@ resp = requests.get(url)
 resp.raise_for_status()
 data = resp.json()
 
-reserved_slim = {}
-for k, v in data["reserved_pvs"].items():
-    pv = int(k)
-    if MIN_PV_ID <= pv <= MAX_PV_ID:
-        title = v[0] if len(v) > 0 else ""
-        username = v[1] if len(v) > 1 else ""
-        reserved_slim[pv] = {
-            "title": title,
-            "username": username
-        }
+reserved_pvs = data["reserved_pvs"]
+uploaded_pvs = ["uploaded_pvs"]
+users = data["users"]
 
+reserved_slim = {}
 used_slim = {}
-for k, v in data["uploaded_pvs"].items():
-    pv = int(k)
-    if MIN_PV_ID <= pv <= MAX_PV_ID:
-        title = v[0] if len(v) > 0 else ""
-        username = v[1] if len(v) > 1 else ""
-        used_slim[pv] = {
-            "title": title,
-            "username": username
-        }
+
+for pv_id_str, info in reserved_pvs.items():
+    pv_id = int(pv_id_str)
+    
+    if not (MIN_PV_ID <= MAX_PV_ID):
+        continue
+        
+    user_id = str(info.get("user"))
+    username = ""
+    
+    if user_id in users:
+        user_info = users[user_id]
+        username = user_info.get("display_name" or user_info.get("name") or "")
+        
+    reserved_slim[pv_id] = {
+        "username": username
+    }
+
+for pv_id_str, entries in uploaded_pvs.items():
+    pv_id = int(pv_id_str)
+    
+    if not (MIN_PV_ID <= MAX_PV_ID):
+        continue
+        
+    if not entries:
+        continue
+        
+    entry = entries[0]
+
+    title = entry.get("name") or entry.get("name_en") or ""
+    uid = str(info.get("udi"))
+    username = ""
+    
+    if udi in users:
+        user_info = users[udi]
+        username = user_info.get("display_name" or user_info.get("name") or "")
+        
+    uploaded_slim[pv_id] = {
+        "title": title
+        "username": username
+    }
 
 with open("pv_ids/reserved_slim.json", "w", encoding="utf8") as f:
     json.dump(reserved_slim, f, indent=2, ensure_ascii=False)
