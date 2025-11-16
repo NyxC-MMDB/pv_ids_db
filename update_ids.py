8bd8674842ac06c40c0158fa647dd36d7eae1a3c
NyxC <esteesunweyrandom@gmail.com>
2025-11-15 20:55:28 -0500

    Update update_ids.py

--- update_ids.py
+++ update_ids.py
@@ -36,29 +36,54 @@ for pv_id_str, info in reserved_pvs.items():
         "username": username
     }
 
-for pv_id_str, entries in uploaded_pvs.items():
-    pv_id = int(pv_id_str)
-    
-    if not (MIN_PV_ID <= MAX_PV_ID):
-        continue
+if isinstance(uploaded_pvs, dict):
+    for pv_id_str, entries in uploaded_pvs.items():
+        pv_id = int(pv_id_str)
         
-    if not entries:
-        continue
+        if not (MIN_PV_ID <= MAX_PV_ID):
+            continue
+            
+        if not entries:
+            continue
+            
+        entry = entries[0]
         
-    entry = entries[0]
-
-    title = entry.get("name") or entry.get("name_en") or ""
-    uid = str(info.get("udi"))
-    username = ""
-    
-    if udi in users:
-        user_info = users[udi]
-        username = user_info.get("display_name" or user_info.get("name") or "")
+        title = entry.get("name") or entry.get("name_en") or ""
+        uid = str(info.get("udi"))
+        username = ""
         
-    uploaded_slim[pv_id] = {
-        "title": title,
-        "username": username
-    }
+        if udi in users:
+            user_info = users[udi]
+            username = user_info.get("display_name" or user_info.get("name") or "")  
+            
+        uploaded_slim[pv_id] = {
+            "title": title,
+            "username": username
+        }
+elif isinstance(uploaded_ids, list):
+    for pv_id_str, entries in uploaded_pvs.items():
+        pv_id = int(pv_id_str)
+        
+        if not (MIN_PV_ID <= MAX_PV_ID):
+            continue
+            
+        if not entries:
+            continue
+            
+        entry = entries[0]
+        
+        title = entry.get("name") or entry.get("name_en") or ""
+        uid = str(info.get("udi"))
+        username = ""
+        
+        if udi in users:
+            user_info = users[udi]
+            username = user_info.get("display_name" or user_info.get("name") or "")  
+            
+        uploaded_slim[pv_id] = {
+            "title": title,
+            "username": username
+        }
 
 with open("pv_ids/reserved_slim.json", "w", encoding="utf8") as f:
     json.dump(reserved_slim, f, indent=2, ensure_ascii=False)
